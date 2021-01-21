97eff06d596f6e33f817503657b4607187c9ab08
joshm1243 <joshuamitchell989@gmail.com>
2021-01-21 15:28:14 +0000

    Updated Views

--- src/user/views.py
+++ src/user/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def login_view(request, *args, **kwargs):
-    return render(request, "index.html")
+    if request.method == "GET":
+        return render(request, "index.html")
