490c5e911f4a31c7336a166f9a1e5fd14869ad5a
joshm1243 <joshuamitchell989@gmail.com>
2021-01-21 15:14:44 +0000

    Pushing updated urls

--- src/lgirproject/urls.py
+++ src/lgirproject/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from user.views import login_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',login_view, name='login'),
 ]
