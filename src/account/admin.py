b2a5b8610b89ac0fc639ae1aff82f9aa87ef8d6d
joshm1243 <joshuamitchell989@gmail.com>
2021-02-11 16:37:59 +0000

    Added The Profiles Table

--- src/account/admin.py
+++ src/account/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Profiles
+admin.site.register(Profiles)
--- src/account/models.py
+++ src/account/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Profiles(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=200)
+    first_name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=60)
