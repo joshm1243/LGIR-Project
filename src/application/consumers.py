24201d5581bcb7a58b2441d24868490d4d568536
joshm1243 <joshuamitchell989@gmail.com>
2021-02-16 14:35:07 +0000

    Added Support For Web-Socket Views

--- src/application/consumers.py
+++ src/application/consumers.py
@@ -1,44 +1,60 @@
 import json
+import redis
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from blog.models import Profile
 
+r = redis.Redis(host="127.0.0.1", port="6379", db=0)
 
 class ApplicationConsumer(WebsocketConsumer):
     def connect(self):
         self.app_code = self.scope["url_route"]["kwargs"]["app_code"]
         self.room_group_name = 'app_%s' % self.app_code
-
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
         )
-
         self.accept()
 
     def disconnecct(self, close_code):
-
         async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,
             self.channel_name
         )
 
+
     def receive(self, text_data=None):
         text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
+        self.data = json.loads(text_data)
 
-        print(message)
+
+        if text_data_json["type"] == "blockly_edit_check":
+
+            blocklyGroupMaster = r.get(self.room_group_name + "_master").decode("utf-8")
+            
+            if blocklyGroupMaster is None or blocklyGroupMaster == "user1":
+
+                #Assign the current user as the master
+                r.set(blocklyGroupMaster,"user1")
+                print(blocklyGroupMaster)
+                
+                self.data = json.loads('{"type" : "blockly_edit_check", "edit" : true}')
+            else:
+                self.data = json.loads('{"type" : "blockly_edit_check", "edit" : false}')
+        
 
         async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name,
+        self.room_group_name,
             {
-                "type" : "chat_message",
-                "message" : message,
+                "type" : "unicast",
                 "sender_channel_name" : self.channel_name
             }
         )
-    
-    def chat_message(self, event):
-        message = event["message"]
 
+    def broadcast(self, event):
         if self.channel_name != event["sender_channel_name"]:
-            self.send(text_data = json.dumps({"message" : message}))
+            self.send(text_data = json.dumps(self.data))
+
+    def unicast(self, event):
+        if self.channel_name == event["sender_channel_name"]:
+            self.send(text_data = json.dumps(self.data))
