ad32380d39ac827becb233984133457b70a41f72
Korbarus <korbarus2951@gmail.com>
2021-02-19 13:36:57 +0000

    Added user and project models

--- src/account/models.py
+++ src/account/models.py
@@ -1,1 +1,22 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+# Create your models here.
+
+class User(AbstractUser):
+    USER_TYPE_CHOICES = (
+      (1, 'default'),
+      (2, 'admin')
+  )
+
+  user_type = models.PositiveSmallIntegerField(choices=USER_TYPE_CHOICES)
+
+# Storing the model for projects here as the two are linked.
+
+class Project(models.Model):
+    name = models.CharField(max_length=30)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='projects')
+    # Other relevant attributes of an appplication go here?
+
+    def __str__(self):
+        return self.name
