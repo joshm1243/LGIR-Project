bfc1c1050eafbe351b202af8cdac9b3541ee61a2
joshm1243 <joshuamitchell989@gmail.com>
2021-02-19 08:02:47 +0000

    [rem] Removed Profile Table Definition

--- src/account/models.py
+++ src/account/models.py
@@ -1,7 +1,1 @@
 from django.db import models
-
-class Profiles(models.Model):
-    username = models.CharField(max_length=100)
-    password = models.CharField(max_length=200)
-    first_name = models.CharField(max_length=60)
-    last_name = models.CharField(max_length=60)
