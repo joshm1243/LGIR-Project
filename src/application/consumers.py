de55b2407f61fbd3e852b3cdfb0d225f91d5002f
joshm1243 <joshuamitchell989@gmail.com>
2021-02-26 16:13:12 +0000

    Added Blockly Blocks JavaScript

--- src/application/consumers.py
+++ src/application/consumers.py
@@ -8,24 +8,19 @@ r = redis.Redis(host="127.0.0.1", port="6379", db=0)
 class ApplicationConsumer(WebsocketConsumer):
     def connect(self):
 
-        try:
-
-            self.app_code = self.scope["url_route"]["kwargs"]["app_code"]
-            self.room_group_name = 'app_%s' % self.app_code
-            async_to_sync(self.channel_layer.group_add)(
-                self.room_group_name,
-                self.channel_name
-            )
-            self.accept()
-        except:
-            print("HH")
+        self.app_code = self.scope["url_route"]["kwargs"]["app_code"]
+        self.room_group_name = 'app_%s' % self.app_code
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
+        self.accept()
 
     def disconnecct(self, close_code):
         async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,
             self.channel_name
         )
-
 
     def receive(self, text_data=None):
         text_data_json = json.loads(text_data)
