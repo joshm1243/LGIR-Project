df25740a5dd5a28bf54f5992ea97bccd7969c0b8
joshm1243 <joshuamitchell989@gmail.com>
2021-03-05 15:05:33 +0000

    Added Project Tiles onto Dashboard

--- src/application/consumers.py
+++ src/application/consumers.py
@@ -3,24 +3,26 @@ import redis
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 
-
+#Connecting to the containerised redis server
 r = redis.Redis(host="127.0.0.1", port="6379", db=0)
 
 
+#A class that represents a single websocket connection
 class ApplicationConsumer(WebsocketConsumer):
-    def connect(self):
+    def connect(self): #The client wishes to connect
 
-        try:
+        #Getting the app identifier from the URL
+        self.app_code = self.scope["url_route"]["kwargs"]["app_code"]
+        self.room_group_name = 'app_%s' % self.app_code
 
-            self.app_code = self.scope["url_route"]["kwargs"]["app_code"]
-            self.room_group_name = 'app_%s' % self.app_code
-            async_to_sync(self.channel_layer.group_add)(
-                self.room_group_name,
-                self.channel_name
-            )
-            self.accept()
-        except:
-            print("HH")
+        #Adding the client to the project room
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        #Accepting the websocket
+        self.accept()
 
     def disconnecct(self, close_code):
         async_to_sync(self.channel_layer.group_discard)(
@@ -29,23 +31,39 @@ class ApplicationConsumer(WebsocketConsumer):
         )
 
 
+    #
     def receive(self, text_data=None):
         text_data_json = json.loads(text_data)
         self.data = json.loads(text_data)
 
         if text_data_json["type"] == "blockly_edit_check":
 
-            self.data = json.loads('{"type" : "blockly_edit_check", "edit" : false}')
-
             async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
                 {
                     "type" : "unicast",
                     "sender_channel_name" : self.channel_name,
-                    "data" : json.loads('{"type" : "blockly_edit_check", "edit" : "fdsafd"}')
+                    "data" : json.loads('{"type" : "blockly_edit_check", "edit" : "true"}')
                 }
             )
+
+        elif text_data_json["type"] == "blockly_edit_request":
+            pass
+        elif text_data_json["type"] == "blockly_edit_request_reply":
+            pass
+        elif text_data_json["type"] == "blockly_serial_send":
             
+            #self.data = json.loads('{"type" : "blockly_serial_send_reply", "msg" : "connecting"}')
+
+            async_to_sync(self.channel_layer.group_send)(
+                self.room_group_name, {
+                                        "type" : "unicast",
+                    "sender_channel_name" : self.channel_name,
+                    "data" : json.loads('{"type" : "blockly_edit_request", "edit" : "true"}')
+                }
+            )
+
+
         else:
 
         #     blocklyGroupMaster = r.get(self.room_group_name + "_master").decode("utf-8")
