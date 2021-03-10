be54138ae4eb02b7cce8a7ece2a013fef4b462a1
Korbarus <korbarus2951@gmail.com>
2021-03-10 17:21:51 +0000

    [change] Fixed indentation error

--- src/application/ws_handlers.py
+++ src/application/ws_handlers.py
@@ -1,2 +1,16 @@
+import redis
+from django.contrib.auth import authenticate
+
+r = redis.Redis(host="127.0.0.1", port="6379", db=0)
+tokendict = {'Name': 'key'}
+
 def AddSocketConnection(key, user):
     print(key, "has been assigned to", user)
+    usertoken = {user: key}
+    tokendict.update(usertoken) #
+    r.lpush('tokenlist', key)   #use hexists to find key?
+
+def CheckAuthentication(user, token)
+    authenticate(self, request, username=none, token=none)
+    
+    
