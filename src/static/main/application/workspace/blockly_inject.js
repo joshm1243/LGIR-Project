de2a14402ce4fd9fd929f5f88f0c44f798e0b083
joshm1243 <joshuamitchell989@gmail.com>
2021-03-14 23:44:36 +0000

    [new] Blockly Workspaces now Mirror

--- src/static/main/application/workspace/blockly_inject.js
+++ src/static/main/application/workspace/blockly_inject.js
@@ -1,5 +1,8 @@
-function createBlockly(options){   //function to create blockly
- 
+//BLOCKLY_INJECT.JS
+//Contains the code used to create a Blockly instance
+
+function createBlockly(options) {
+
   var blocklyArea = document.getElementById('blockly-container');   //variable blocklyarea that holds the container
   var blocklyDiv = document.getElementById('blocklyDiv');   //blockly div
   var ws = new Blockly.WorkspaceSvg(new Blockly.Options({}))   //variable to hold workspace svg with blockly options
@@ -12,8 +15,8 @@ function createBlockly(options){   //function to create blockly
   //   options = {readOnly: true}
     
   // }
-  var workspace = Blockly.inject(blocklyDiv,   // variable that holds a blockly inject
-      options);
+  var workspace = Blockly.inject(blocklyDiv, options);
+
    
   var onresize = function(e) {  
     // Compute the absolute coordinates and dimensions of blocklyArea.
@@ -31,15 +34,11 @@ function createBlockly(options){   //function to create blockly
     blocklyDiv.style.width = blocklyArea.offsetWidth + 'px';
     blocklyDiv.style.height = blocklyArea.offsetHeight + 'px';
     Blockly.svgResize(workspace);
-    if (isCurrentEditor = false){ 
-    
-    }
-    
+
   };
   window.addEventListener('resize', onresize, false);  //window event listener 
   onresize();
   Blockly.svgResize(workspace);   //resizes the workspace
-
 
   document.getElementById("toggle-chat").addEventListener("click", function() { //gets chat to open on click 
     onresize();
@@ -49,44 +48,28 @@ function createBlockly(options){   //function to create blockly
     onresize();
   });
 
-  workspace.addChangeListener(function(event){  //add change listener to check if socket is open
-    if(socket.OPEN){
-
-      //Preventing user interface 
-      if (event instanceof Blockly.Events.UiBase) {  
-        return;
-      }
-
-      var json = event.toJson();  //json to show if blockly content has been made
-      socket.send(JSON.stringify({
-        "type" : "blockly_edit_has_been_made",
-        "blockly_content" : json
-      }))
-    }  
-  });
-
-  socket.onmessage = function(event) {   //when a blockly has been put into workspace is will send this message
-    let data = JSON.parse(event.data)
-    if (data.type == "blockly_edit_has_been_made") {
-      var blocklyContent = data.blockly_content
-   
-      var blocklyEvent = Blockly.Events.fromJson(blocklyContent, workspace);  //if blockly content is added says which one it is in console
-      blocklyEvent.run(true);
-      console.log(blocklyEvent)
+  workspace.addChangeListener(function(event) {
+    if (event instanceof Blockly.Events.UiBase) {
+      return
     }
-  }
- 
+
+    var json = event.toJson();
+
+    SocketSend({
+      "type" : "blockly_edit_has_been_made",
+      "blockly_content" : json
+    })
+
+  })
+
+  return workspace
+
 }
-function deleteBlockly(workspace){  //function to delete blockly block
+
+function deleteBlockly(workspace) {
   workspace.dispose()
 }
 
-// //socket.onopen = function(workspace){
-//   if(socket.OPEN){
-//     //workspace.addChangeListener(function(){
-//       console.log("what am i even doing?")
-//     };
-//   //} 
 
-   
+
 
