198286ec2de41f09291670ab0464dcc2dcc170e9
Korbarus <korbarus2951@gmail.com>
2021-03-17 22:00:44 +0000

    [change] Changed formatting of chat model in admin

--- src/lgirproject/settings.py
+++ src/lgirproject/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -128,9 +127,9 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_L10N = True
+USE_L10N = False
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
