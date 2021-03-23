46f85a942009f235573f8719f1a09444a96c3c12
LuisNicholas <Luis.Nicholas64@gmail.com>
2021-03-23 16:47:27 +0000

    [new] Finished HTML + CSS + JS for settings page

--- src/application/routing.py
+++ src/application/routing.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .consumers import ApplicationConsumer
+from .settingsconsumer import SettingsConsumer
 
 ws_urlpatterns = [
     path('application/<app_code>/workspace/', ApplicationConsumer.as_asgi()),
+    path('application/<app_code>/settings/', ApplicationConsumer.as_asgi()),
 ]
